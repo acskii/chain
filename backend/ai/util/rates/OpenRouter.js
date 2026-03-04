2f1526e35ffd3bab7022988b99e4bcb4fcb9febd
Andrew Sameh Adel <andrew.sameh.adel@gmail.com>
2026-03-04 09:29:30 +0200

    feat: check rate limit for ai endpoints

---
+++ backend/ai/util/rates/OpenRouter.js
@@ -0,0 +1,28 @@
+/*
+    Implementation of rate limit and usage limit checking for OpenRouter
+*/
+
+import getUsageData from "./General";
+
+export function getRemainingUsage(apiKey) {
+    // Get general response from endpoint
+    const response = getUsageData(apiKey);
+
+    // Only return the remaining usage of the API key
+    const { limit_remaining } = response.data;
+
+    return limit_remaining ?? 999;  // 999 means infinite
+};
+
+export function getInfoUsage(apiKey) {
+    // Get general response from endpoint
+    const response = getUsageData(apiKey);
+
+    // Return the usage of the API key
+    // Check: https://openrouter.ai/docs/api/reference/limits
+    const { limit, limit_remaining, is_free_tier } = response.data;
+
+    return {
+        limit, limit_remaining, is_free_tier
+    }
+}
