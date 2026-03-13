8f6dcec32689a369d0df25f33907b03fca8511b9
Andrew Sameh Adel <andrew.sameh.adel@gmail.com>
2026-03-13 14:24:43 +0200

    feat: add identity route for user

--- backend/user/route.js
+++ backend/user/route.js
@@ -4,8 +4,15 @@ import express from "express";
 import { register, login } from "./controllers/email.js";
 import { auth, callback } from "./controllers/google.js";
 import oauth from "./controllers/oauth.js";
+import getProfile from "./controllers/profile.js";
+
+/* Authorisation */
+import verify from "../middleware/auth.js";
 
 const router = express.Router();
+
+/* /profile */
+router.get('/profile', verify, getProfile);
 
 /* /register */
 router.post('/register', register);
